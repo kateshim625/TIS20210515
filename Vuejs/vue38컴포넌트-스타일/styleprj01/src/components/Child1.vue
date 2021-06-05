46e852aed912a5db56d7d46cb1036b69cdf56f5e
sfsdf <fdafadf>
2021-06-05 15:17:40 +0900

    2021-06-05 수업

---
+++ Vuejs/vue38컴포넌트-스타일/styleprj01/src/components/Child1.vue
@@ -0,0 +1,29 @@
+<style scoped>
+/**스콥트를 사용하면 동일 클레스를 사용해도 v-가 생겨서 충돌을 피하게해준다 */
+.main {
+    border: 1px solid black;
+    background-color: yellow;
+}
+</style>
+
+<template>
+    <div class="main">
+        {{ msg }}
+        <child11 />
+        {{ msg }}
+    </div>
+</template>
+<script>
+import Child11 from "./Child11.vue";
+export default {
+    components: {
+        Child11: Child11
+    },
+    data: function () {
+        return {
+            msg: "Child1"
+        };
+    }
+};
+</script>
+
---
+++ Vuejs/vue38컴포넌트-스타일/styleprj01/src/components/Child11.vue
@@ -0,0 +1,19 @@
+            <style scoped>
+.test {
+    font-style: italic;
+}
+</style>
+            <template>
+    <div class="test">
+        <h3>{{ msg }}</h3>
+    </div>
+</template>
+<script cript>
+export default {
+    data: function () {
+        return {
+            msg: "Child - Child"
+        };
+    }
+};
+</script>
